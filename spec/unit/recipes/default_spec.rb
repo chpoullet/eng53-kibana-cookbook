5666d7a50652c051e621890d99988d03d57971aa
Abdimalik Mao <58399886+AMao7@users.noreply.github.com>
2020-03-25 16:23:33 +0000

    Update default_spec.rb

--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -34,5 +34,14 @@ describe 'kibana_cookbook::default' do
     it 'should create a kibana.yml templates in /etc/kibana/kibana.yml' do
       expect(chef_run).to create_template "/etc/kibana/kibana.yml"
     end
+    it 'should add elasticsearch key' do
+      expect(chef_run).to execute 'wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -'
+    end
+    it 'should add elasticsearch repository' do
+      expect(chef_run).to execute 'sudo add-apt-repository "deb https://artifacts.elastic.co/packages/7.x/apt stable main"'
+    end
+    it 'should fix template' do
+      expect(chef_run).to execute 'sudo chmod go-w /etc/kibana/kibana.yml'
+    end
   end
 end
